bb29159ab9bda406b8bf4acd5420e8781f164435
Evan McGrane <evanmcgranebb98@gmail.com>
2025-11-25 01:49:38 +0000

    block

--- src/blockchain/consensus/clique/index.ts
+++
@@ -1,5 +0,0 @@
-// src/blockchain/consensus/clique/index.ts
-export * from "./types";
-export * from "./utils";
-export * from "./clique";
-
---
+++ src/testdata/index.ts
@@ -0,0 +1,4 @@
+export * from './blocks/index.ts'
+export * from './chainConfigs/index.ts'
+export * from './gethGenesis/index.ts'
+export * from './signers.ts'
